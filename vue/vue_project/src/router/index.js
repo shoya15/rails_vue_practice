8a648a1aa9b5ccd26e5b1276a24d7ba3fd5c2bdf
shoya15 <s.h.rainight@gmail.com>
2023-07-02 20:29:33 +0900

    Update: ログインによるアクセス制限を追加した

--- vue/vue_project/src/router/index.js
+++ vue/vue_project/src/router/index.js
@@ -4,9 +4,9 @@ import TopPage from '@/views/TopPage'
 import SamplePage from '@/views/SamplePage'
 import SignUpPage from '@/views/SignUpPage'
 import SignInPage from '@/views/SignInPage'
+import store from '@/store'
 
 Vue.use(VueRouter)
-
 const routes = [
   {
     path: '/',
@@ -36,4 +36,13 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if(store.state.user && (to.name === "SignUpPage" || to.name === "SignInPage")){
+    next({ name: "TopPage" });
+  }else if(!store.state.user &&  (to.name !== "SignInPage" && to.name !== "SignUpPage")){
+    next(false);
+  }else{
+    next();
+  }
+})
 export default router
