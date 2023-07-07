b0d84943bcb6dc8aeb9b78d47702148a6f7c7d0a
shoya15 <s.h.rainight@gmail.com>
2023-07-07 11:30:42 +0900

    Add: 完了済みのタスクと未完了のタスクをそれぞれ表示できるようにした

---
+++ vue/vue_project/src/components/AllTasks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="task in tasks" :key="task.id" @update="updateHandler">
+      {{ task.content }}
+      ← created by user:
+      {{ task.user_id }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  methods: {
+    updateHandler(task) {
+      this.$emit("update", task);
+    }
+  }
+}
+</script>
---
+++ vue/vue_project/src/components/TasksStatus.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="status flex-item">
+    <span v-for="status in Object.keys(STATUSES)" :key="status">
+      <input type="radio" :id="status" v-model="selectedStatus" :value="status" @click="updateStatusButton" />
+      <label :for="status">{{ status }}</label>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedStatus: "未完了",
+    };
+  },
+  computed: {
+    STATUSES() {
+      return {
+        完了済み: true,
+        未完了: null
+      };
+    }
+  },
+  methods: {
+    updateStatusButton() {
+      this.$emit("button_status", !this.STATUSES[this.selectedStatus]);
+    }
+  }
+};
+</script>
