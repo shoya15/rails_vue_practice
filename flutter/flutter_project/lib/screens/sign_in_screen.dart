b05b360569db1012a7369548d8eac5d79eaf5e88
shoya15 <s.h.rainight@gmail.com>
2023-08-02 07:00:12 +0900

    Add: サインイン/サインアウト機能を実装した

---
+++ flutter/flutter_project/lib/screens/sign_in_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import '../api/communication.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  String nameOrEmail = "";
+  String password = "";
+  Map currentUser = {};
+
+  @override
+  Widget build(BuildContext context) {
+    Communication communication = Communication();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign In')),
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 12,
+          horizontal: 36,
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              TextField(
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black
+                ),
+                decoration: const InputDecoration(
+                  hintText: 'ユーザー名またはメールアドレスを入力してください',
+                ),
+                onChanged: (value) => {
+                  setState(() {
+                    nameOrEmail = value;
+                  })
+                }
+              ),
+            
+              TextField(
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Colors.black
+                ),
+                decoration: const InputDecoration(
+                  hintText: 'パスワードを入力してください',
+                ),
+                onChanged: (value) => {
+                  setState((){
+                    password = value;
+                  })
+                }
+              ),
+
+              OutlinedButton(
+                child: const Text('サインインする'),
+                onPressed: () async {
+                  dynamic res = await communication.post('/api/sign_in', { 
+                    'session': {
+                      'name_or_email': nameOrEmail,
+                      'password': password
+                    }
+                  });
+                  if(res.statusCode == 200) {
+                    setState(() {
+                      currentUser = jsonDecode(res.body)['session'];
+                    });
+                    Navigator.pop(context, currentUser);
+                  }
+                }
+              )
+            ]
+          )
+        )
+      )
+    );
+  }
+}
--- rails/rails_project/app/controllers/csrf_tokens_controller.rb
+++ rails/rails_project/app/controllers/csrf_tokens_controller.rb
@@ -1,6 +1,6 @@
 class CsrfTokensController < ApplicationController
   def show
     response.set_header('X-CSRF-Token', form_authenticity_token)
-    render_success
+    render_success headers: form_authenticity_token
   end
 end
